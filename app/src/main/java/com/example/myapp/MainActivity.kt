505d696dc61b62ee12345c9cb00cb5b4abf39177
muskanaggarwal <muskan.aggarwal@housing.com>
2023-07-05 15:38:26 +0530

    calculator

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.myapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.myapp.ui.theme.MyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyAppTheme {
+                startActivity(Intent(applicationContext,CalcActivity::class.java))
+//                MyApp(this)
+                // A surface container using the 'background' color from the theme
+//                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+//                    Greeting("Android")
+//                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp(mainActivity: MainActivity) {
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var onClickSignup by remember {
+        mutableStateOf(false)
+    }
+    Box() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Yellow)
+        ) {
+            TextField(value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") })
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") })
+            Row(){
+                Button(onClick = { onClickSignup=true }) {
+                    Text("Sign Up")
+                }
+                Button(onClick = { Toast.makeText(mainActivity,"Logged In Successfully",Toast.LENGTH_SHORT).show()}) {
+                    Text("Login")
+
+                }
+
+            }
+            if(onClickSignup){
+                Text(email, fontSize = 30.sp)
+            }
+        }
+    }
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/myapp/practiceKotlin.kt
@@ -0,0 +1,19 @@
+package com.example.myapp
+
+import androidx.compose.runtime.Composable
+
+class practiceKotlin() {
+
+    fun add(n1:Int,n2:Int):Int{
+       return n1+n2
+    }
+    fun sub(n1:Int,n2:Int):Int{
+        return n1-n2
+    }
+    fun mul(n1:Int,n2:Int):Int{
+        return n1*n2
+    }
+    fun div(n1:Int,n2:Int):Int{
+        return n1/n2
+    }
+}
