f15195a168890b41020bf1057f541a02a285e7c8
muskanaggarwal <muskan.aggarwal@housing.com>
2023-07-21 17:00:28 +0530

    remove api-keys

--- app/src/main/java/com/example/myapp/apiservice/TrainAPIService.kt
+++ app/src/main/java/com/example/myapp/apiservice/TrainAPIService.kt
@@ -11,8 +11,8 @@ import retrofit2.http.POST
 interface TrainAPIService {
     @POST("/")
     suspend fun getTrainDetails(@Body trainRequest: TrainRequest,
-                           @Header("X-RapidAPI-Key")    apiKey : String="4df8556d2bmsh2df755e28304a87p1b7a42jsn0164cab5f4ea",
-                           @Header("X-RapidAPI-Host")   apiHost:String ="trains.p.rapidapi.com"):ArrayList<TrainDetailsResponse>
+                                @Header("X-RapidAPI-Key")    apiKey : String= "APIKEY",
+                                @Header("X-RapidAPI-Host")   apiHost:String ="trains.p.rapidapi.com"):ArrayList<TrainDetailsResponse>
 
     companion object {
         var apiService: TrainAPIService? = null
--- app/src/main/java/com/example/myapp/apiservice/WeatherAPIService.kt
+++ app/src/main/java/com/example/myapp/apiservice/WeatherAPIService.kt
@@ -11,7 +11,7 @@ interface WeatherAPIService {
 
     @GET("current.json")
     suspend fun getWeather( @Query("q") location:String,
-                            @Header("X-RapidAPI-Key")    apiKey : String="4df8556d2bmsh2df755e28304a87p1b7a42jsn0164cab5f4ea",
+                            @Header("X-RapidAPI-Key")    apiKey : String="APIKEY",
                             @Header("X-RapidAPI-Host")   apiHost:String ="weatherapi-com.p.rapidapi.com"):WeatherResponse
 
     companion object {
